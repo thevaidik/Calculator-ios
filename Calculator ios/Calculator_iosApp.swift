1882633e09eeaaa3c4458224ba5b53e21355a872
thevaidik <vaidik50000@gmail.com>
2024-02-14 20:01:05 +0530

    Initial Commit

---
+++ Calculator ios/Calculator_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calculator_iosApp.swift
+//  Calculator ios
+//
+//  Created by Vaidik Dubey on 14/02/24.
+//
+
+import SwiftUI
+
+@main
+struct Calculator_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
