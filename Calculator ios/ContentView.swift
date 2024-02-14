b1411002c469d1f50143802ca810235b0148d34c
thevaidik <vaidik50000@gmail.com>
2024-02-14 20:19:47 +0530

    adding intial buttons setup

--- Calculator ios/ContentView.swift
+++ Calculator ios/ContentView.swift
@@ -9,13 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color.black.edgesIgnoringSafeArea(.all)
+            VStack {
+                //test display
+                HStack{
+                    Spacer()
+                    Text("0")
+                        .bold()
+                        .font(.system(size:52))
+                        .foregroundColor(.white)
+                }
+                
+                
+                
+                
+                
+                // our buttons
+                
+            }
+
         }
-        .padding()
+        
     }
 }
 
