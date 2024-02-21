feb3c686b45d2c5b28e6ee653ffcdad74b377794
thevaidik <vaidik50000@gmail.com>
2024-02-21 10:43:23 +0530

    buttons setup

--- Calculator ios/ContentView.swift
+++ Calculator ios/ContentView.swift
@@ -7,33 +7,90 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        ZStack {
+enum CalcButton: String 
+{
+    case one = "1"
+    case two = "2"
+    case three = "3"
+    case four = "4"
+    case five = "5"
+    case six = "6"
+    case seven = "7"
+    case eight = "8"
+    case nine = "9"
+    case zero = "0"
+    case add = "+"
+    case subtract = "-"
+    case multiply = "*"
+    case divide = "/"
+    case decimal = "."
+    case percentage = "%"
+    case equal = "="
+    case clear = "AC"
+    case negative = "-/+"
+    
+}
+
+struct ContentView: View 
+{
+    let buttons: [[CalcButton]] = [
+        [.clear , .negative, .percentage , .divide ],
+        [.seven , .eight, .nine ,.subtract ],
+        [.four , .five, .six ,.multiply ],
+        [.one , .two, .three , .add],
+        [.zero , .decimal, .equal ],
+    ]
+    var body: some View
+    {
+        ZStack
+        {
             Color.black.edgesIgnoringSafeArea(.all)
-            VStack {
+            VStack
+            {
                 //test display
-                HStack{
+                HStack
+                {
                     Spacer()
                     Text("0")
                         .bold()
-                        .font(.system(size:52))
+                        .font(.system(size: 64))
                         .foregroundColor(.white)
                 }
+                .padding()
                 
-                
-                
-                
-                
-                // our buttons
+                //our buttona
+                ForEach(buttons, id : \.self ) { row in
+                    HStack
+                    {
+                        ForEach(row , id: \.self.rawValue ) { item in
+                            Button(action :
+                                    {
+                                
+                            }, label :
+                                    {
+                                Text(item.rawValue)
+                                    .font(.system(size:32))
+                                    .frame(width : 70, height : 70 )
+                                    .background(Color.orange)
+                                    .foregroundColor(.white)
+                                    .cornerRadius(35)
+                                
+                            }
+                            )
+                            
+                        }
+                    }
+                }
                 
             }
-
+            
         }
         
     }
+    
 }
-
 #Preview {
     ContentView()
 }
+
+
